9292151dc6abc308874005a1a6d953c498d2b9a1
Matias98cs <matias98cs@gmail.com>
2023-02-11 19:05:06 -0300

    pokemons realizado

--- src/screens/Pokemon.tsx
+++ src/screens/Pokemon.tsx
@@ -1,7 +1,90 @@
+import { useState } from "react";
+
+const POKEMONS = [
+  "bulbasaur",
+  "ivysaur",
+  "venusaur",
+  "charmander",
+  "charmeleon",
+  "charizard",
+  "squirtle",
+  "wartortle",
+  "blastoise",
+  "caterpie",
+  "metapod",
+  "butterfree",
+  "weedle",
+  "kakuna",
+  "beedrill",
+  "pidgey",
+  "pidgeotto",
+  "pidgeot",
+  "rattata",
+  "raticate",
+  "spearow",
+  "fearow",
+  "ekans",
+  "arbok",
+  "pikachu",
+  "raichu",
+  "sandshrew",
+  "sandslash"
+];
+
+const MATCH = Math.floor(Math.random() * POKEMONS.length);
+
+type Form = HTMLFormElement & {
+  pokemon: HTMLInputElement;
+};
+
 export default function Pokemon() {
-    return (
-        <div>
-            <h1>Pokemon</h1>
-        </div>
-    )
+  const [hasWon, toggleWon] = useState(false);
+
+  function handleSubmit(event: React.FormEvent<Form>) {
+    event.preventDefault();
+
+    const { pokemon } = event.currentTarget;
+
+    if (pokemon.value.toLowerCase() === POKEMONS[MATCH]) {
+      toggleWon(true);
+      alert(`You Won!`);
+    } else {
+      alert(`Wrong answer!`);
+    }
+  }
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: 12,
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <img
+        width={412}
+        height={412}
+        style={{
+          imageRendering: "pixelated",
+          filter: hasWon ? "" : "brightness(0) invert(1)",
+        }}
+        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${
+          MATCH + 1
+        }.png`}
+        alt=""
+      />
+      {hasWon ? (
+        <button style={{ width: "100%" }} onClick={() => location.reload()}>
+          Play Again
+        </button>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="pokemon" autoFocus/>
+          <button type="submit">Submit</button>
+        </form>
+      )}
+    </div>
+  );
 }
