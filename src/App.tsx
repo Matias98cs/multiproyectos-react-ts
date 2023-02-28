7121b78874a33bbc3dd342906d5485989c923b50
matias98csNotebook <matias98cs@gmail.com>
2023-02-28 15:10:53 -0300

    home creada que muestra las paginas de los proyectos

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom"
 import Memotest from "./screens/Memotest"
 import WordsPerMinute from "./screens/WordsPerMinute"
 import Pokemon from "./screens/Pokemon"
+import Home from "./screens/Home"
 
 
 function App() {
 
   return (
     <Routes>
+      <Route path="/" element={<Home/>} />
       <Route path="/memotest" element={<Memotest />} />
       <Route path="/wpm" element={<WordsPerMinute />} />
       <Route path="/pokemon" element={<Pokemon />} />
